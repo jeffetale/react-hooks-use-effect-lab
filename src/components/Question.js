45e4486f90fa136cd4ef249cf4a0f2cab5aa4eac
jeff <jeff.etale@student.moringaschool.com>
2023-07-22 12:14:26 +0300

    completed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,39 @@
-import React, { useState } from "react";
+import React, {  useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    let countDownTimer;
+
+    //decrease time
+    const decreaseTimer = () => {
+      setTimeRemaining((currentTime) => currentTime - 1); 
+      console.log("decreasing time")
+    };
+
+    //start time
+    countDownTimer = setInterval(decreaseTimer, 1000);
+
+    //clean up interval
+    return () => {
+    clearInterval(countDownTimer);
+    console.log('Interval cleared')
+    };
+  }, []);
+
+  useEffect(() => {
+    // reset time to ten when it hits zero
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      //Initiate onAnswered callback "false"
+      onAnswered(false);
+
+      console.log('reset occured')
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
